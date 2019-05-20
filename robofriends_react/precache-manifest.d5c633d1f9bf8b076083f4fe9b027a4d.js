84c8f4be9070e31c8e81f76160d851014fd02171
Jurgen <nnjaco@gmail.com>
2019-05-20 14:14:20 -0500

    added robofriends to the project

---
+++ robofriends_react/precache-manifest.d5c633d1f9bf8b076083f4fe9b027a4d.js
@@ -0,0 +1,41 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2632cad35385dcf28c3106b56afba2d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f68128fcf7703b0d4b34",
+    "url": "/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "73e2419b954de7722c9d",
+    "url": "/static/css/main.94a4de15.chunk.css"
+  },
+  {
+    "revision": "f68128fcf7703b0d4b34",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "73e2419b954de7722c9d",
+    "url": "/static/js/main.591f2021.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "url": "/static/media/ab83b275830b2d585ea027c47e3b237a.39cdb6e5.woff"
+  },
+  {
+    "url": "/static/media/ab83b275830b2d585ea027c47e3b237a.49aec4af.woff2"
+  },
+  {
+    "url": "/static/media/ab83b275830b2d585ea027c47e3b237a.ab83b275.ttf"
+  },
+  {
+    "url": "/static/media/ab83b275830b2d585ea027c47e3b237a.b3aca254.eot"
+  },
+  {
+    "url": "/static/media/ab83b275830b2d585ea027c47e3b237a.fd159673.svg"
+  }
+]);
