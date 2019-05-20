65861cb0e440fc42c2f1630d4960c59193dc78ec
Jurgen <nnjaco@gmail.com>
2019-05-20 14:34:09 -0500

    deploy robofriends 1.0

---
+++ robofriends/precache-manifest.dc9a7acf51ceee71b96df907abf2dd95.js
@@ -0,0 +1,41 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cfeaa527e374811f1dd4f967281196",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "f68128fcf7703b0d4b34",
+    "url": "/robofriends/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "703125ecc890eecf53ab",
+    "url": "/robofriends/static/css/main.13b7aeb3.chunk.css"
+  },
+  {
+    "revision": "f68128fcf7703b0d4b34",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "703125ecc890eecf53ab",
+    "url": "/robofriends/static/js/main.4fb97b90.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "url": "/robofriends/static/media/ab83b275830b2d585ea027c47e3b237a.39cdb6e5.woff"
+  },
+  {
+    "url": "/robofriends/static/media/ab83b275830b2d585ea027c47e3b237a.49aec4af.woff2"
+  },
+  {
+    "url": "/robofriends/static/media/ab83b275830b2d585ea027c47e3b237a.ab83b275.ttf"
+  },
+  {
+    "url": "/robofriends/static/media/ab83b275830b2d585ea027c47e3b237a.b3aca254.eot"
+  },
+  {
+    "url": "/robofriends/static/media/ab83b275830b2d585ea027c47e3b237a.fd159673.svg"
+  }
+]);
