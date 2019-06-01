bb338bb4360f07e66bdbd3ee10406a6919df791e
jurgen <nnjaco@gmail.com>
2019-06-01 18:48:00 -0500

    updated face recognition app

--- face_recognition/service-worker.js
+++ face_recognition/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/face_recognition/precache-manifest.0cf0650fde361aa6b0ff02c909ef858a.js"
+  "/face_recognition/precache-manifest.cbef4e3ebe1c287530a1070ab9231ebe.js"
 );
 
 self.addEventListener('message', (event) => {
