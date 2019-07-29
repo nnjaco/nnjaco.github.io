25f6036578158eeb6d0e20c36c9260c28b5ed330
Jurgen <nnjaco@gmail.com>
2019-07-29 13:29:52 -0500

    fixed json fil

--- web-lacost/service-worker.js
+++ web-lacost/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.d2acb253ad05c885f9036fee4879f8a5.js"
+  "/web-lacost/precache-manifest.8a4ea3ad1d02862e6bfed98876191145.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-lacost/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
