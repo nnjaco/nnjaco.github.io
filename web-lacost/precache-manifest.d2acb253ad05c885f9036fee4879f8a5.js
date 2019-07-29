70dc5ead448683021f48f8ea9c34343ba4fee863
Jurgen <nnjaco@gmail.com>
2019-07-29 13:21:53 -0500

    added web-lacost project

---
+++ web-lacost/precache-manifest.d2acb253ad05c885f9036fee4879f8a5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b021e7da5d2715f1bfa314ded73e97",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8253caadee1b43a50c9e",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "799b5be31cf987ffbbc3",
+    "url": "/robofriends/static/css/main.52904d97.chunk.css"
+  },
+  {
+    "revision": "8253caadee1b43a50c9e",
+    "url": "/robofriends/static/js/2.585cc8ad.chunk.js"
+  },
+  {
+    "revision": "799b5be31cf987ffbbc3",
+    "url": "/robofriends/static/js/main.3cb387c5.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "7c8e2ae95be7375fb57636a325a3e48a",
+    "url": "/robofriends/static/media/0.7c8e2ae9.png"
+  },
+  {
+    "revision": "cda5fc98206d754b863c66da75fafd91",
+    "url": "/robofriends/static/media/0.cda5fc98.png"
+  },
+  {
+    "revision": "e7b780289b100a02a75ea24f156391c1",
+    "url": "/robofriends/static/media/0.e7b78028.png"
+  },
+  {
+    "revision": "3ca0e9beaa77b8982526ba2d92b97b8b",
+    "url": "/robofriends/static/media/1.3ca0e9be.png"
+  },
+  {
+    "revision": "6129f86600d837e5cda3e7e90e4f357d",
+    "url": "/robofriends/static/media/1.6129f866.png"
+  },
+  {
+    "revision": "eda816403956feea753cfc9b2a7bd32d",
+    "url": "/robofriends/static/media/1.eda81640.png"
+  },
+  {
+    "revision": "2a248d4c70b04003e890f3a84ea46e41",
+    "url": "/robofriends/static/media/2.2a248d4c.png"
+  },
+  {
+    "revision": "889842ee717f870c99dbf2c8bce22f23",
+    "url": "/robofriends/static/media/2.889842ee.png"
+  },
+  {
+    "revision": "905caf1c58c50851339d2e5d079b7096",
+    "url": "/robofriends/static/media/2.905caf1c.png"
+  },
+  {
+    "revision": "65c08d855271482e93370bfca0fe6296",
+    "url": "/robofriends/static/media/3.65c08d85.png"
+  },
+  {
+    "revision": "dfc88669735630d5a9309dba77d4a129",
+    "url": "/robofriends/static/media/4.dfc88669.png"
+  },
+  {
+    "revision": "f9786dc04cef0436a22faed6951b0956",
+    "url": "/robofriends/static/media/bg-image.f9786dc0.jpg"
+  },
+  {
+    "revision": "3c50a0cea2c8305d4bd6d8243f39a762",
+    "url": "/robofriends/static/media/logo_lacost_800X334.3c50a0ce.png"
+  },
+  {
+    "revision": "0d5d4f02963be4d8a40525c768d68ff9",
+    "url": "/robofriends/static/media/logo_lacost_textonly.0d5d4f02.png"
+  },
+  {
+    "revision": "87fce4486376e6a7a3dc2af9ad7e04e2",
+    "url": "/robofriends/static/media/nav_icon.87fce448.svg"
+  }
+]);
